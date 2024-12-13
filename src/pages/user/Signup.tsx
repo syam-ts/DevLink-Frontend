042cb19b859b65e6900bd9ce97b96c7e7b1399e7
| ｓｙａｍ | <syamnandhu3@gmail.com>
2024-12-13 10:13:16 +0530

    signup finish

--- src/pages/user/Signup.tsx
+++ src/pages/user/Signup.tsx
@@ -1,11 +1,42 @@
+import {useState} from 'react';
+import axios from 'axios';
 
 
 const Signup = () => {
 
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+    email: "",
+    mobile: 0
+  })
+
+  const handleChange = (e: any) => {
+  
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  }
+
+  const handleSubmit = async () => {
+       
+    console.log('Form ', formData)
+    const response = await axios.post('http://localhost:3000/user/signup', formData)
+    console.log(response.data.message);
+   try {
+    
+   } catch (err) {
+     console.error('ERROR: ',err)
+   }
+  }
+ 
+
     return ( 
   
-        <div>
-             <div className="font-[sans-serif]">
+ <div>
+   <div className="font-[sans-serif]">
       <div className="grid lg:grid-cols-2 md:grid-cols-2 items-center gap-4">
         <div className="max-md:order-1 h-screen min-h-full">
           <img src="https://assets-us-01.kc-usercontent.com/7beb5311-75a4-0049-50f5-8f58fd55aba7/7a1ab39f-e032-47b9-aaa9-ab06f301ac80/Article_59.jpeg" className="w-full h-full object-cover" alt="login-image" />
@@ -13,14 +44,14 @@ const Signup = () => {
 
         <form className="max-w-xl w-full p-6 mx-auto">
           <div className="mb-12">
-            <h3 className="text-gray-800 text-4xl font-extrabold">Sign in</h3>
-            <p className="text-gray-800 text-sm mt-6">Don't have an account <a href="javascript:void(0);" className="text-blue-600 font-semibold hover:underline ml-1 whitespace-nowrap">Register here</a></p>
+            <h3 className="text-gray-800 text-4xl font-extrabold">DevLink</h3>
+            <p className="text-gray-800 text-sm mt-6">Already have an account <a href="javascript:void(0);" className="text-blue-600 font-semibold hover:underline ml-1 whitespace-nowrap">Login here</a></p>
           </div>
 
           <div>                   
             <label className="text-gray-800 text-sm block mb-2">Username</label>
             <div className="relative flex items-center">
-              <input name="email" type="text" required className="w-full text-sm text-gray-800 border-b border-gray-300 focus:border-blue-600 px-2 py-3 outline-none" placeholder="Enter email" />
+              <input onChange={ handleChange } name="username" type="text" required className="w-full text-sm text-gray-800 border-b border-gray-300 focus:border-blue-600 px-2 py-3 outline-none" placeholder="Enter username" />
               <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" className="w-[18px] h-[18px] absolute right-2" viewBox="0 0 682.667 682.667">
                 <defs>
                   <clipPath id="a" clipPathUnits="userSpaceOnUse">
@@ -35,7 +66,7 @@ const Signup = () => {
             </div>
             <label className="text-gray-800 text-sm block mb-2">Mobile</label>
             <div className="relative flex items-center">
-              <input name="email" type="text" required className="w-full text-sm text-gray-800 border-b border-gray-300 focus:border-blue-600 px-2 py-3 outline-none" placeholder="Enter email" />
+              <input name="mobile" type="text" onChange={ handleChange } required className="w-full text-sm text-gray-800 border-b border-gray-300 focus:border-blue-600 px-2 py-3 outline-none" placeholder="Enter mobile" />
               <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" className="w-[18px] h-[18px] absolute right-2" viewBox="0 0 682.667 682.667">
                 <defs>
                   <clipPath id="a" clipPathUnits="userSpaceOnUse">
@@ -50,7 +81,7 @@ const Signup = () => {
             </div>
             <label className="text-gray-800 text-sm block mb-2">Email</label>
             <div className="relative flex items-center">
-              <input name="email" type="text" required className="w-full text-sm text-gray-800 border-b border-gray-300 focus:border-blue-600 px-2 py-3 outline-none" placeholder="Enter email" />
+              <input name="email" type="text" onChange={handleChange} required className="w-full text-sm text-gray-800 border-b border-gray-300 focus:border-blue-600 px-2 py-3 outline-none" placeholder="Enter email" />
               <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" className="w-[18px] h-[18px] absolute right-2" viewBox="0 0 682.667 682.667">
                 <defs>
                   <clipPath id="a" clipPathUnits="userSpaceOnUse">
@@ -68,7 +99,7 @@ const Signup = () => {
           <div className="mt-8">
             <label className="text-gray-800 text-sm block mb-2">Password</label>
             <div className="relative flex items-center">
-              <input name="password" type="password" required className="w-full text-sm text-gray-800 border-b border-gray-300 focus:border-blue-600 px-2 py-3 outline-none" placeholder="Enter password" />
+              <input name="password" onChange={handleChange} type="password" required className="w-full text-sm text-gray-800 border-b border-gray-300 focus:border-blue-600 px-2 py-3 outline-none" placeholder="Enter password" />
               <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" className="w-[18px] h-[18px] absolute right-2 cursor-pointer" viewBox="0 0 128 128">
                 <path d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z" data-original="#000000"></path>
               </svg>
@@ -76,12 +107,7 @@ const Signup = () => {
           </div>
 
           <div className="flex flex-wrap items-center justify-between gap-4 mt-6">
-            <div className="flex items-center">
-              <input id="remember-me" name="remember-me" type="checkbox" className="h-4 w-4 shrink-0 text-blue-600 focus:ring-blue-500 border-gray-300 rounded" />
-              <label className="ml-3 block text-sm text-gray-800">
-                Remember me
-              </label>
-            </div>
+    
             <div>
               <a href="jajvascript:void(0);" className="text-blue-600 font-semibold text-sm hover:underline">
                 Forgot Password?
@@ -90,8 +116,8 @@ const Signup = () => {
           </div>
 
           <div className="mt-12">
-            <button type="button" className="w-full py-2.5 px-4 text-sm tracking-wide rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none">
-              Sign in
+            <button onClick={handleSubmit} type="button" className="w-full py-2.5 px-4 text-sm tracking-wide rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none">
+              Sign up
             </button>
           </div>
 
@@ -128,6 +154,8 @@ const Signup = () => {
       </div>
     </div>
  
+ 
+
  </div>
  
     )
