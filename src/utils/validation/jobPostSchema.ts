a931baacdeda54457823adb18090db0f90801fb2
| ｓｙａｍ | <syamnandhu3@gmail.com>
2025-02-14 10:28:01 +0530

    fix: draftjob schema estimate time validation

--- src/utils/validation/jobPostSchema.ts
+++ src/utils/validation/jobPostSchema.ts
@@ -1,5 +1,6 @@
 import * as yup from "yup";
 
+
 export const jobPostSchema = yup.object().shape({
     title: yup
         .string()
@@ -8,6 +9,7 @@ export const jobPostSchema = yup.object().shape({
         .max(30, "Must be under 30 characters")
         .required("Title is required"),
 
+
     keyResponsiblities: yup
         .string()
         .trim()
@@ -15,38 +17,38 @@ export const jobPostSchema = yup.object().shape({
         .max(60, "KeyResponsiblities should under 60")
         .required("KeyResponsiblities is required"),
 
-        requiredSkills: yup
-        .array()  
-        .min(3,"Minimum 3 skills required")
-        .max(10,"Maximum 10 skills are allowed")
+
+    requiredSkills: yup
+        .array()
+        .min(3, "Minimum 3 skills required")
+        .max(10, "Maximum 10 skills are allowed")
         .required("Skills are required"),
 
-  
 
     paymentType: yup
-    .string()
-    .oneOf(["hourly", "fixed"], "Payment type must be either 'hourly' or 'fixed'")
-    .required("Payment type is required"),
+        .string()
+        .oneOf(["hourly", "fixed"], "Payment type must be either 'hourly' or 'fixed'")
+        .required("Payment type is required"),
+
 
     payment: yup
-    .number()
-    .transform((originalValue) => (originalValue && !isNaN(originalValue) ? Number(originalValue) : NaN))
-    .typeError("Payment must be a valid number")
-    .required("Payment is required")
-    .when("paymentType", {
-      is: "hourly",
-      then: (schema) => schema.min(100, "Hourly rate must be at least 100rs").max(1500, "Hourly rate must be at most 1500rs"),
-      otherwise: (schema) => schema.min(2000, "Fixed price must be at least 2000rs").max(70000, "Fixed price must be at most 70000rs"),
-    }),
-  
- 
- 
+        .number()
+        .transform((originalValue) => (originalValue && !isNaN(originalValue) ? Number(originalValue) : NaN))
+        .typeError("Payment must be a valid number")
+        .required("Payment is required")
+        .when("paymentType", {
+            is: "hourly",
+            then: (schema) => schema.min(100, "Hourly rate must be at least 100rs").max(1500, "Hourly rate must be at most 1500rs"),
+            otherwise: (schema) => schema.min(2000, "Fixed price must be at least 2000rs").max(70000, "Fixed price must be at most 70000rs"),
+        }),
+
 
     maxProposals: yup
         .number()
         .min(3, "Minimum 3 proposals are mandatory")
         .max(10, "Maximum proposals are 10")
         .required("Maximum proposal is required"),
+
 
     description: yup
         .string()
@@ -56,17 +58,15 @@ export const jobPostSchema = yup.object().shape({
         .required("Description is required"),
 
 
- 
-
-        estimateTime: yup
+    estimateTime: yup
         .number()
         .transform((originalValue) => (originalValue && !isNaN(originalValue) ? Number(originalValue) : NaN))
         .typeError("Estimate time must be a valid number")
         .required("Estimate time is required")
         .when("paymentType", {
-          is: "hourly",
-          then: (schema) => schema.min(5, "Estimate time must be at least 5hr").max(48, "Estimate time must be at most 48hrs"),
-          otherwise: (schema) => schema.min(10, "Estimate time must be at least 10hr").max(120, "Estimate time must be at most 120hrs"),
+            is: "hourly",
+            then: (schema) => schema.min(5, "Estimate time must be at least 5hr").max(48, "Estimate time must be at most 48hrs"),
+            otherwise: (schema) => schema.min(10, "Estimate time must be at least 10hr").max(120, "Estimate time must be at most 120hrs"),
         }),
 
     location: yup
