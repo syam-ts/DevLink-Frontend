d9ba727de587959e0d299516675fdb2aa7d20607
| ｓｙａｍ | <syamnandhu3@gmail.com>
2025-02-23 10:44:18 +0530

    feat: add remove wihslist

--- src/components/common/MonoJobPost.tsx
+++ src/components/common/MonoJobPost.tsx
@@ -1,44 +1,43 @@
-import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
-import { JobProposalModal } from "../shadcn/modal/JobProposalModal"; 
-import { apiUserInstance } from '.././../api/axiosInstance/axiosUserInstance';
-import { apiClientInstance } from '.././../api/axiosInstance/axiosClientRequest';
-import useUserVerified  from "../../hooks/userUserVerified";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { JobProposalModal } from "../shadcn/modal/JobProposalModal";
+import { apiUserInstance } from ".././../api/axiosInstance/axiosUserInstance";
+import { apiClientInstance } from ".././../api/axiosInstance/axiosClientRequest";
+import useUserVerified from "../../hooks/userUserVerified";
+import { toast } from "sonner";
+import { Sonner } from "../sonner/Toaster";
 
+type Id = string;
 
 interface JobPost {
-  _id: string
-  title: string
-  amount: number
-  clientId: string
-  estimateTime: string
-  description: string
-  estimateTimeinHours: number
-  keyResponsibilities: string[]
-  requiredSkills: string[]
-  location: string
-  expertLevel: string
-  maxProposals: number
-  proposalCount: number
-  projectType: string
-  paymentType: string
-  isPayment: boolean
-  status: string
-  createdAt: string
+  _id: Id;
+  title: string;
+  amount: number;
+  clientId: Id;
+  estimateTime: string;
+  description: string;
+  estimateTimeinHours: number;
+  keyResponsibilities: string[];
+  requiredSkills: string[];
+  location: string;
+  expertLevel: string;
+  maxProposals: number;
+  proposalCount: number;
+  projectType: string;
+  paymentType: string;
+  isPayment: boolean;
+  status: string;
+  createdAt: string;
 }
-
 
 interface FormData {
-  bidAmount: number
-  bidDeadline: number
-  description: string
-  paymentType: string
+  bidAmount: number;
+  bidDeadline: number;
+  description: string;
+  paymentType: string;
 }
 
-
-
 const MonoJobPost = () => {
-
   const [jobPost, setJobPost]: any = useState<JobPost>({
     _id: "",
     title: "",
@@ -57,221 +56,277 @@ const MonoJobPost = () => {
     paymentType: "",
     isPayment: false,
     status: "",
-    createdAt: ""
+    createdAt: "",
   });
 
   const [formData, setFormData] = useState<FormData>({
     bidAmount: 0,
     bidDeadline: 0,
     description: "",
-    paymentType: ""
+    paymentType: "",
   });
-  const { jobPostId, type } = useParams();
- 
-let userVerified;
-  if(type === 'user-view') {
-    userVerified = useUserVerified(); 
+  const { jobPostId, type } = useParams<{ jobPostId: Id; type: string }>();
+
+  let userVerified;
+  if (type === "user-view") {
+    userVerified = useUserVerified();
   }
- 
 
   useEffect(() => {
     try {
-
       (async () => {
         let response: any;
-        if (type === 'user-view') {
+        if (type === "user-view") {
           response = await apiUserInstance.get(`/job-view/${jobPostId}`);
-          console.log('THe resp ', response)
           setJobPost(response?.data?.jobPost);
-        } else if(type === 'client-view') {
+        } else if (type === "client-view") {
           response = await apiClientInstance.get(`/job-view/${jobPostId}`);
-          console.log('reosnpse cli: ', response)
           setJobPost(response?.data?.jobPost);
         }
       })();
     } catch (err: any) {
-      console.error('ERROR: ', err.message);
+      console.error("ERROR: ", err.message);
     }
   }, []);
 
-
   // updating formdata -------
   useEffect(() => {
-
     setFormData({
       bidAmount: jobPost?.amount,
       bidDeadline: jobPost?.estimateTimeinHours,
       description: "",
-      paymentType: jobPost?.paymentType
-    })
+      paymentType: jobPost?.paymentType,
+    });
   }, [jobPost]);
 
+  const addToWishlist = async (jobPostId: Id) => {
+    try {
+      const { data } = await apiUserInstance.post("/addToWishlist", {
+        jobPostId,
+      });
 
+      if (!data.success) {
+       console.log('ERROR: ',data.message);
+      } else {
+      toast.success("Post added to wishlist", {
+        style: {
+          backgroundColor: "#28bd26",
+          color: "white",
+          width: "12rem",
+          height: "2.9rem",
+        },
+        position: "top-center",
 
+      }) 
+      }
+    } catch (err: any) {
+      console.log("ERROR: ", err.response.data.message);
+      toast.error(err.response.data.message, {
+        style: {
+          backgroundColor: "yellow",
+          color: "Black",
+          width: "12rem",
+          height: "2.9rem",
+        },
+        position: "top-center",
+
+      })
+    }
+  };
 
   return (
     <>
-      <div className='h-screen mx-auto w-2/3 py-28 comfortaa-regular'>
+    <Sonner />
+      <div className="h-screen mx-auto w-2/3 my-28 comfortaa-regular">
         <section>
-          <span className='text-2xl'>{jobPost?.title}</span>
-          <div className='flex gap-20 py-4 text-xs'>
+          <span className="text-2xl">{jobPost?.title}</span>
+          <div className="flex gap-20 py-4 text-xs">
             <span>Posted {jobPost?.estimateTimeinHours} hours ago</span>
-            <div className='flex '>
-              <img className='w-5 h-5 mx-2' src='https://cdn-icons-png.flaticon.com/128/927/927667.png' />
+            <div className="flex ">
+              <img
+                className="w-5 h-5 mx-2"
+                src="https://cdn-icons-png.flaticon.com/128/927/927667.png"
+              />
               <span>{jobPost?.location}</span>
             </div>
           </div>
         </section>
 
         {/* Desctipion section */}
-        <section className='py-6'>
-          <span className='text-sm'>
-            {jobPost?.description}
-          </span>
+        <section className="py-6">
+          <span className="text-sm">{jobPost?.description}</span>
         </section>
 
         <section>
-          <div className='py-10 flex flex-wrap w-2/3 gap-x-64 gap-y-20 '>
-            <div className='text-xs'>
-              <div className='flex gap-3 pb-2'>
-                <img className='w-5 h-5' src='https://cdn-icons-png.flaticon.com/128/5579/5579107.png' alt='price-image' />
+          <div className="py-10 flex flex-wrap w-2/3 gap-x-64 gap-y-20 ">
+            <div className="text-xs">
+              <div className="flex gap-3 pb-2">
+                <img
+                  className="w-5 h-5"
+                  src="https://cdn-icons-png.flaticon.com/128/5579/5579107.png"
+                  alt="price-image"
+                />
                 <span> ₹{jobPost?.amount}.00 </span>
               </div>
-              <span className='px-4'> {jobPost?.paymentType} Price </span>
+              <span className="px-4"> {jobPost?.paymentType} Price </span>
             </div>
 
-            <div className='text-xs'>
-              <div className='flex gap-3 pb-2'>
-                <img className='w-5 h-5' src='https://cdn-icons-png.flaticon.com/128/1491/1491165.png' alt='price-image' />
+            <div className="text-xs">
+              <div className="flex gap-3 pb-2">
+                <img
+                  className="w-5 h-5"
+                  src="https://cdn-icons-png.flaticon.com/128/1491/1491165.png"
+                  alt="price-image"
+                />
                 <span> Estimate Time </span>
               </div>
-              <span className='px-4'> {jobPost?.estimateTimeinHours}/hr </span>
+              <span className="px-4"> {jobPost?.estimateTimeinHours}/hr </span>
             </div>
 
-            <div className='text-xs'>
-              <div className='flex gap-3 pb-2'>
-                <img className='w-5 h-5' src='https://cdn-icons-png.flaticon.com/128/1491/1491165.png' alt='price-image' />
+            <div className="text-xs">
+              <div className="flex gap-3 pb-2">
+                <img
+                  className="w-5 h-5"
+                  src="https://cdn-icons-png.flaticon.com/128/1491/1491165.png"
+                  alt="price-image"
+                />
                 <span> Expertise </span>
               </div>
-              <span className='px-4'> {jobPost?.expertLevel} </span>
+              <span className="px-4"> {jobPost?.expertLevel} </span>
             </div>
 
-            <div className='text-xs'>
-              <div className='flex gap-3 pb-2'>
-                <img className='w-5 h-5' src='https://cdn-icons-png.flaticon.com/128/927/927667.png' alt='price-image' />
+            <div className="text-xs">
+              <div className="flex gap-3 pb-2">
+                <img
+                  className="w-5 h-5"
+                  src="https://cdn-icons-png.flaticon.com/128/927/927667.png"
+                  alt="price-image"
+                />
                 <span> Location </span>
               </div>
-              <span className='px-4'> {jobPost?.location} </span>
+              <span className="px-4"> {jobPost?.location} </span>
             </div>
 
-            <div className='text-xs'>
-              <div className='flex gap-3 pb-2'>
-                <img className='w-5 h-5' src='https://cdn-icons-png.flaticon.com/128/5530/5530083.png' alt='price-image' />
+            <div className="text-xs">
+              <div className="flex gap-3 pb-2">
+                <img
+                  className="w-5 h-5"
+                  src="https://cdn-icons-png.flaticon.com/128/5530/5530083.png"
+                  alt="price-image"
+                />
                 <span> {jobPost?.projectType} </span>
               </div>
-              <span className='px-4'> Project Type </span>
+              <span className="px-4"> Project Type </span>
             </div>
 
-            <div className='text-xs'>
-              <div className='flex gap-3 pb-2'>
-                <img className='w-5 h-5' src='https://cdn-icons-png.flaticon.com/128/18510/18510347.png' alt='price-image' />
+            <div className="text-xs">
+              <div className="flex gap-3 pb-2">
+                <img
+                  className="w-5 h-5"
+                  src="https://cdn-icons-png.flaticon.com/128/18510/18510347.png"
+                  alt="price-image"
+                />
                 <span> Teah Stack </span>
               </div>
-              <span className='px-4'> {jobPost?.domain || '.........'}  </span>
+              <span className="px-4"> {jobPost?.domain || "........."} </span>
             </div>
           </div>
         </section>
 
         {/* Skills section */}
-        <section className='pt-10'>
-          <span >Skills and Expertise</span>
-          <div className='flex gap-4'>
-            {
-              jobPost?.requiredSkills?.map((skill: string) => (
-
-                <span className="rounded-full border border-transparent my-4 py-2 px-8  text-center text-sm transition-all text-slate-900  bg-slate-300 focus:bg-slate-100 active:bg-slate-100 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" >
-                  {skill}
-                </span>
-              ))
-            }
-
+        <section className="pt-10">
+          <span>Skills and Expertise</span>
+          <div className="flex gap-4">
+            {jobPost?.requiredSkills?.map((skill: string) => (
+              <span className="rounded-full border border-transparent my-4 py-2 px-8  text-center text-sm transition-all text-slate-900  bg-slate-300 focus:bg-slate-100 active:bg-slate-100 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+                {skill}
+              </span>
+            ))}
           </div>
-          <div className='text-xs mt-3'>
-            {jobPost?.keyResponsiblities}
-          </div>
+          <div className="text-xs mt-3">{jobPost?.keyResponsiblities}</div>
         </section>
 
-        <section className='pt-10'>
+        <section className="pt-10">
           <span>Activity on this job</span>
-          <div className='text-xs flex gap-10 pt-3'>
-            <div className='flex gap-3'>
-              <span>
-                Proposals:
-              </span>
-              <img className='w-5 h-5' src='https://cdn-icons-png.flaticon.com/128/10995/10995709.png' alt='question-image' />
+          <div className="text-xs flex gap-10 pt-3">
+            <div className="flex gap-3">
+              <span>Proposals:</span>
+              <img
+                className="w-5 h-5"
+                src="https://cdn-icons-png.flaticon.com/128/10995/10995709.png"
+                alt="question-image"
+              />
               <span>{jobPost?.proposalCount} of 10</span>
             </div>
-            <div className='flex gap-3'>
-              <span>
-                Boosted Proposals:
-              </span>
-              <img className='w-5 h-5' src='https://cdn-icons-png.flaticon.com/128/10995/10995709.png' alt='question-image' />
+            <div className="flex gap-3">
+              <span>Boosted Proposals:</span>
+              <img
+                className="w-5 h-5"
+                src="https://cdn-icons-png.flaticon.com/128/10995/10995709.png"
+                alt="question-image"
+              />
               <span>3</span>
             </div>
           </div>
         </section>
 
         {/* About Client */}
-        <section className='pt-10'>
+        <section className="pt-10">
           <span>About The Client</span>
-          <p className='text-xs'>Member since may 23 2024</p>
-          <div className='flex gap-10 text-xs '>
+          <p className="text-xs">Member since may 23 2024</p>
+          <div className="flex gap-10 text-xs ">
             <span>{jobPost?.aboutClient?.location}</span>
             <span>₹{jobPost?.aboutClient?.totalSpend}.00 total spend</span>
             <span>{jobPost?.aboutClient?.totalHours} hours</span>
             <span>{jobPost?.aboutClient?.domain}</span>
-            <span>Large Company({jobPost?.aboutClient?.numberOfEmployees} people)</span>
+            <span>
+              Large Company({jobPost?.aboutClient?.numberOfEmployees} people)
+            </span>
           </div>
-          <div className='pt-4'>
-            <span className='text-sm'>{jobPost?.aboutClient?.companyName} Inc.</span>
+          <div className="pt-4">
+            <span className="text-sm">
+              {jobPost?.aboutClient?.companyName} Inc.
+            </span>
           </div>
         </section>
 
         {/* job apply section */}
-        <section className='mt-10'>
-
+        <section className="mt-10">
           <hr />
-          <p className='text-sm'> Intrested </p>
-          {
-            userVerified && (
+          <div>
+            <p className="text-sm"> Intrested </p>
+          </div>
+          {userVerified && (
+            <div className="flex ">
               <button className="" type="button">
-                <JobProposalModal jobPostId={jobPost?._id}
-                  formData={formData} setFormData={setFormData} paymentType={jobPost?.paymentType} />
+                <JobProposalModal
+                  jobPostId={jobPost?._id}
+                  formData={formData}
+                  setFormData={setFormData}
+                  paymentType={jobPost?.paymentType}
+                />
               </button>
-            )
-          }
 
-          {
-            userVerified && (
-              <div>
-                {/* <button className="rounded-xl bg-[#0000ff] py-3 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2" type="button">
-                    Save for later
-                   </button> */}
-              </div>
-            )
-          }
-
+              <button
+                onClick={() => addToWishlist(jobPost?._id)}
+                className="rounded-xl bg-[#0000ff] py-3 px-4 font-extrabold border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
+                type="button"
+              >
+                Save for later
+              </button>
+            </div>
+          )}
         </section>
 
         {/* Similar Jobs */}
-        <section className='my-20 '>
+        <section className="my-20 ">
           <div>
-            <span className='text-2xl'>Related Job Posts</span>
+            <span className="text-2xl">Related Job Posts</span>
           </div>
         </section>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default MonoJobPost
+export default MonoJobPost;
--- src/components/common/Wishlist.tsx
+++ src/components/common/Wishlist.tsx
@@ -1,50 +1,78 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { apiUserInstance } from "../../api/axiosInstance/axiosUserInstance";
+import { toast } from "sonner";
+import { Sonner } from "../sonner/Toaster";
 
+const Wishlist: React.FC = () => {
 
-
-const Wishlist = () => {
-  const [jobs, setJobs] = useState([]);
+  const [jobs, setJobs] = useState({});
+  const [wishlistId, setWishlistId] = useState<string>("");
+  const [removeFlag, setRemoveFlag] = useState<boolean>(false);
 
   useEffect(() => {
     try {
       (async () => {
         const { data } = await apiUserInstance.get("/wishlist-view");
-        setJobs(data.wishlist[0].jobPostData);
+        setJobs(data.wishlist.jobPostData);
+        console.log("twh: ", data.wishlist.jobPostData);
+        setWishlistId(data.wishlist._id);
       })();
+      setRemoveFlag(false);
     } catch (err: any) {
       console.log("ERROR: ", err.message);
     }
-  }, []); 
+  }, [removeFlag]);
 
-  const removeWishlistjob = async() => {
-    try{
+  const removeFromWishlist = async (wishlistId: string, jobPostId: string) => {
+    try {
+      const { data } = await apiUserInstance.patch("/removeFromWishlist", {
+        wishlistId,
+        jobPostId,
+      });
 
-    }catch(err: any) {
-      console.log('ERROR: ',err.message);
+      if (data.success) {
+        setRemoveFlag(true);
+        toast.success("Post removed", {
+          style: {
+            backgroundColor: "#28bd26",
+            color: "white",
+            width: "12rem",
+            height: "2.9rem",
+          },
+          position: "top-center",
+        });
+      }
+    } catch (err: any) {
+      console.log("ERROR: ", err.message);
     }
-  }
+  };
+
+  console.log('jobs: ',Object.entries(jobs).length)
+
 
   return (
     <>
-    <section>
-      <div className='flex justify-center my-10'>
-      <span className='arsenal-sc-regular text-3xl '> Wishlist </span>
-      </div>
-      <hr className='w-2/3 mx-auto' />
-    </section>
-      {jobs?.map((job: any) => (
+      <Sonner />
+      <section>
+        <div className="flex justify-center my-10">
+          <span className="arsenal-sc-regular text-3xl "> Wishlist </span>
+        </div>
+        <hr className="w-2/3 mx-auto" />
+      </section>{
+        Object.entries(jobs).length != 0 ? (
+          <div>
+ {Object.entries(jobs)?.map((job: any) => (
         <div className="w-2/3 border-gray-100 shadow-xl rounded-xl h-[300px] border mx-auto my-20 p-12 arsenal-sc-regular">
           <div className="flex justify-between ">
             <div className="grid">
-              <span className="text-2xl text-start">{job?.title}</span>
-              <span className="text-sm mt-2">{job?.description}</span>
+              <span className="text-2xl text-start">{job[1]?.title}</span>
+              <span className="text-sm mt-2">{job[1]?.description}</span>
               <div className="grid justify-start gap-3 mt-3">
-                <span className="text-sm">{job?.expertLevel}</span>
-                <span className="text-sm">{job?.location}</span>
+                <span className="text-sm">{job[1]?.expertLevel}</span>
+                <span className="text-sm">{job[1]?.location}</span>
               </div>
               <span className="flex gap-3">
-                {job?.requiredSkills?.map((skill: string) => (
+                {job[1]?.requiredSkills?.map((skill: string) => (
                   <span className="rounded-full border border-transparent my-4 py-1.5 px-8  text-center text-sm transition-all text-white bg-[#0000ff] focus:bg-slate-100 active:bg-slate-100 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
                     {skill}
                   </span>
@@ -53,13 +81,14 @@ const Wishlist = () => {
             </div>
 
             <div className="grid text-end py-3">
-              <span className="text-sm">{job?.amount}.00₹</span>
-              <span className="text-sm">{job?.paymentType}</span>
-              <span className="text-sm">{job?.estimateTimeinHours}/hr</span>
+              <span className="text-sm">{job[1]?.amount}.00₹</span>
+              <span className="text-sm">{job[1]?.paymentType}</span>
+              <span className="text-sm">{job[1]?.estimateTimeinHours}/hr</span>
               <span className="text-sm text-green-400 underline">
-                {job?.projectType}
+                {job[1]?.projectType}
               </span>
               <button
+                onClick={() => removeFromWishlist(wishlistId, job[1]?._id)}
                 className="rounded-md bg-black px-12 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
                 type="button"
               >
@@ -69,6 +98,14 @@ const Wishlist = () => {
           </div>
         </div>
       ))}
+          </div>
+        ) : (
+          <div className='flex justify-center my-[15rem]'>
+             <p className='arsenal-sc-regular text-2xl '>Your Wishlist is Empty</p>
+          </div>
+        )
+      }
+     
     </>
   );
 };
