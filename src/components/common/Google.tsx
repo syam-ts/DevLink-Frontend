5ad8dcaba5a293733be385dc801f397eecdd1e43
| ｓｙａｍ | <syamnandhu3@gmail.com>
2025-03-03 22:01:33 +0530

    fix: user google login fix

--- src/components/common/Google.tsx
+++ src/components/common/Google.tsx
@@ -1,44 +1,60 @@
-import { GoogleLogin } from '@react-oauth/google';
-import { jwtDecode } from "jwt-decode"; 
-import { useNavigate } from 'react-router-dom';
-import { signInUser } from '../../redux/slices/userSlice' 
-import { useDispatch } from 'react-redux';
-import config from '../../config/helper/config'
-import axios from 'axios';
+import { GoogleLogin } from "@react-oauth/google";
+import { jwtDecode } from "jwt-decode";
+import { useNavigate } from "react-router-dom";
+import { signInUser } from "../../redux/slices/userSlice";
+import { useDispatch } from "react-redux";
+import config from "../../config/helper/config";
+import axios from "axios";
 
-const Google = ({role}: any) => {
+interface GoogleProps {
+  role: string
+};
 
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
+const Google: React.FC<GoogleProps> = ({ role }) => {
+  const navigate = useNavigate(),
+    dispatch = useDispatch();
 
-    return (
-        <GoogleLogin
-            onSuccess={async (credentialResponse: any) => {
-                try {
-                    let credentialResponseDecode: any = jwtDecode(credentialResponse.credential)
-                    let data = {email: credentialResponseDecode.email, name: credentialResponseDecode.given_name, password: credentialResponseDecode.jti}
-                console.log(credentialResponseDecode.email, 'and ',credentialResponseDecode.name , 'whole data : ', credentialResponseDecode);
-                const response = await axios.post(`${config.VITE_SERVER_URL}/${role}/googleLogin`,data,  {
-                    withCredentials: true,  
-                  } );
-                console.log(response.data.message);
-                 if(response.data.success) {
-                       dispatch(signInUser(response.data))
-                    navigate(`/${role}/home`);
-                 }
-                } catch (error) {
-                    console.log('ERROR: ',error);
-                }
+  return (
+    <GoogleLogin
+      onSuccess={async (credentialResponse: any) => {
+        try {
+          interface Credentials {
+            email: string
+            given_name: string
+            jti: string
+          };
+          let credentialResponseDecode: Credentials = jwtDecode(
+            credentialResponse.credential
+          );
+          let data = {
+            email: credentialResponseDecode.email,
+            name: credentialResponseDecode.given_name,
+            password: credentialResponseDecode.jti,
+          };
 
-            }}
+          const response = await axios.post(
+            `${config.VITE_SERVER_URL}/${role}/googleLogin`,
+            data,
+            {
+              withCredentials: true,
+            }
+          ); 
 
-            
-            onError={() => {
-                console.log('Login Failed');
-            }}
-        />
-    )
-}
-
+          if (response.data.success) {
+            const { accessToken } = response.data;
+            localStorage.setItem("accessToken", accessToken);
+            dispatch(signInUser(response.data.user));
+            navigate(`/${role}/home`);
+          }
+        } catch (error) {
+          console.log("ERROR: ", error);
+        }
+      }}
+      onError={() => {
+        console.log("Login Failed");
+      }}
+    />
+  );
+};
 
 export default Google;
