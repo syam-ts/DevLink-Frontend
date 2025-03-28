9f58bfd6c534a5c1db69c1c21d65755e39d9ebf9
| ｓｙａｍ | <syamnandhu3@gmail.com>
2025-03-29 00:59:10 +0530

    refactor: add env's on the configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+ 
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+  return {
+    plugins: [react()],
+    define: {
+      'import.meta.env.VITE_BASE_URL': JSON.stringify(env.VITE_BASE_URL),
+      'import.meta.env.VITE_SERVER_URL': JSON.stringify(env.VITE_SERVER_URL),
+      'import.meta.env.VITE_STRIPE_PUBLISABLE_KEY': JSON.stringify(env.VITE_STRIPE_PUBLISABLE_KEY),
+      'import.meta.env.VITE_GEMINI_CHATBOT_API_KEY': JSON.stringify(env.VITE_GEMINI_CHATBOT_API_KEY),
+      'import.meta.env.VITE_CLOUDINARY_URL': JSON.stringify(env.VITE_CLOUDINARY_URL),
+    },
+  };
+});
