892d24da95f2744ab00dc0bb490503a665878b74
| ｓｙａｍ | <syamnandhu3@gmail.com>
2025-04-02 00:32:34 +0530

    refactor: modified add new components to the landing page

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -4,7 +4,7 @@
 const Footer = () => {
 
     return (
-                <footer className="bg-blueGray-200 mt-96 pt-64 pb-6 arsenal-sc-regular">
+                <footer className="bg-stone-300 mt-44 pt-44 pb-6 arsenal-sc-regular text-black">
                     <div className="container mx-auto px-4">
                         <div className="flex flex-wrap text-left lg:text-left">
                             <div className="w-full lg:w-6/12 px-4">
@@ -13,10 +13,10 @@ const Footer = () => {
                                     Find us on like these platforms, we respond 1-2 business days.
                                 </h5>
                                 <div className="mt-6 lg:mb-0 mb-6">
-                                    <button className="bg-[#0000ff] text-lightBlue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
-                                        <i className="fab fa-twitter"></i></button><button className="bg-[#0000ff] text-lightBlue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
-                                        <i className="fab fa-facebook-square"></i></button><button className="bg-[#0000ff] text-pink-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
-                                        <i className="fab fa-dribbble"></i></button><button className="bg-[#0000ff] text-blueGray-800 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
+                                    <button className="bg-white text-lightBlue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
+                                        <i className="fab fa-twitter"></i></button><button className="bg-white text-lightBlue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
+                                        <i className="fab fa-facebook-square"></i></button><button className="bg-white text-pink-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
+                                        <i className="fab fa-dribbble"></i></button><button className="bg-white text-blueGray-800 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
                                         <i className="fab fa-github"></i>
                                     </button>
                                 </div>
@@ -24,7 +24,7 @@ const Footer = () => {
                             <div className="w-full lg:w-6/12 px-4">
                                 <div className="flex flex-wrap items-top mb-6">
                                     <div className="w-full lg:w-4/12 px-4 ml-auto">
-                                        <span className="block uppercase text-[#0000ff] text-sm font-semibold mb-2">Useful Links</span>
+                                        <span className="block uppercase text-black text-sm font-semibold mb-2">Useful Links</span>
                                         <ul className="list-unstyled">
                                             <li>
                                                 <a className="text-black no-underline hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://www.creative-tim.com/presentation?ref=njs-profile">About Us</a>
@@ -41,7 +41,7 @@ const Footer = () => {
                                         </ul>
                                     </div>
                                     <div className="w-full lg:w-4/12 px-4">
-                                        <span className="block uppercase text-[#0000ff] text-sm font-semibold mb-2">Other Resources</span>
+                                        <span className="block uppercase text-black text-sm font-semibold mb-2">Other Resources</span>
                                         <ul className="list-unstyled">
                                             <li>
                                                 <a className="text-black no-underline hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://github.com/creativetimofficial/notus-js/blob/main/LICENSE.md?ref=njs-profile">MIT License</a>
@@ -67,7 +67,7 @@ const Footer = () => {
                                         Copyright ©
                                         <span id="get-current-year">2024</span>
                                         <a href="https://www.creative-tim.com/product/notus-js" className="text-blueGray-500 hover:text-gray-800" target="_blank"/> Devlink JS by
-                                            <a href="https://www.creative-tim.com?ref=njs-profile" className="text-blueGray-500 hover:text-blueGray-800">Syamdev</a>.
+                                            <a href="https://www.creative-tim.com?ref=njs-profile" className="text-black hover:text-blueGray-800">Syamdev</a>.
                                     </div>
                                 </div>
                             </div>
--- src/pages/publicPages/LandingPage.tsx
+++ src/pages/publicPages/LandingPage.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom"; 
+import { useNavigate } from "react-router-dom";
 import { Input } from "../../components/shadcn/Input";
 import Footer from "../../components/common/Footer";
 
@@ -11,92 +11,111 @@ const LandingPage = () => {
 
   return (
     <div>
-      <nav className="bg-white shadow-md arsenal-sc-regular">
-        <div className="flex flex-1 sm:items-stretch sm:justify-center py-3 ml-12">
-        
-            <span className="font-bold text-center text-2xl py-1 nunito-regular">DevLink</span>
-       
-          {/* <div className=" sm:ml-6 sm:block ">
-            <div className="flex space-x-24 pr-44">
-              <span className="p-2 text-sm comfortaa-regular text-gray-950">
-                About
-              </span>
-              <span className="p-2 text-sm comfortaa-regular text-gray-950">
-                Contact
-              </span>
-            </div>
-          </div> */}
+      <nav>
+        <div className="flex flex-1 sm:items-stretch sm:justify-center py-3 bg-[#2b3247] w-full"> 
+          <span className="font-bold text-center text-white text-2xl py-1 nunito-regular">DevLink</span>
         </div>
       </nav>
 
-      <section className="text-center pt-12 my-6">
-        <span className="text-3xl font-serif">
-          Hiring The Best , Make Us Proud And Joy
-        </span>
-      </section>
-
-      <section className="flex justify-between py-12 mx-auto w-[1100px] my-20 border border-black rounded-xl">
-        <div className="pl-12">
-          <img src="http://localhost:5173/public/landingpage_left.png" alt="profile-image" />
-        </div>
-        <div className=" ">
-          <div className="pt-44">
-            <div>
-              <span className="font-mono text-xl">We connect people to</span>
-            </div>
-            <span className="font-mono text-xl">bring projects to life</span>
-          </div>
-
-          <div className="">
-            <button className="pt-4">
-              <Input type="email" placeholder="search" />
-            </button>
-          </div>
-        </div>
-
-        <div className="pr-12">
-          <img src="http://localhost:5173/landingpage_right.png" alt="profile-image" />
-        </div>
-      </section>
-
-      <section className="text-center pt-12 my-6">
-        <span className="text-3xl font-serif">Unlimited Oppurtunities...</span>
-      </section>
-
-      <section className="flex mt-20">
-        <div className="ml-80">
-          <img src="/lasdingpage_option.png" alt="join-image" />
-        </div>
-        <div>
-          <img src="/ladnigpage_last-img.png" alt="join-image" />
-        </div>
-      </section>
-
-      <section className="flex p-20 mt-28 arsenal-sc-regular">
-        <div className="ml-44">
-          <img src="public/landingpage_bigimage.png" alt="professional image" />
-        </div>
-
-        <div className="text-center mx-auto mt-44">
-          <span className="font-bold text-4xl">Bussiness Elavators and</span>
-          <div>
-            <span className="font-bold text-4xl">Carrer Crafters</span>
-          </div>
-          <span className="pt-12 grid text-2xl">
-            Our being able to do what we like best, <br />
-            every pleasure is to be welcomed and every
-            <br />
-            pain avoided but in certain circumstances and <br />
-            owing to the claims of duty or the obligations.
+      <section>
+        <div className="text-center pt-12 my-6 arsenal-sc-regular">
+          <span className="text-3xl">
+            Hiring The Best , Make Us Proud And Joy
           </span>
-          <button
-            onClick={toRolePage}
-            className="bg-blue-700 w-44 rounded-2xl text-white font-bold hover:bg-green-500 h-12 mt-12"
-          >
-            Get Started
-          </button>
         </div>
       </section>
+
+      <section>
+        <div className="flex flex-col md:flex-row justify-center items-center py-12 :mx-auto mx-auto max-w-[1100px] my-20 border border-black rounded-xl px-6 md:px-12">
+          <div className="w-full md:w-1/3 flex justify-center md:justify-start">
+            <img
+              src="http://localhost:5173/public/landingpage_left.png"
+              alt="profile-image"
+              className="max-w-[80%] md:max-w-full h-auto"
+            />
+          </div>
+
+          <div className="w-full md:w-1/3 text-center md:text-left flex flex-col items-center md:items-start">
+            <div className="pt-12 md:pt-52 justify-center flex flex-wrap">
+              <div>
+                <span className="arsenal-sc-regular text-lg md:text-xl ">We connect people to</span>
+              </div>
+              <span className="arsenal-sc-regular text-lg md:text-xl">bring projects to life</span>
+            </div>
+          </div>
+
+          <div className="w-full md:w-1/3 flex justify-center md:justify-end">
+            <img
+              src="http://localhost:5173/landingpage_right.png"
+              alt="profile-image"
+              className="max-w-[80%] md:max-w-full h-auto"
+            />
+          </div>
+        </div>
+      </section>
+
+
+
+      <section>
+        <div className="text-center pt-12 my-6">
+          <span className="text-3xl arsenal-sc-regular">Unlimited Oppurtunities...</span>
+        </div>
+      </section>
+
+      <section>
+        <div className='flex mt-20 justify-end bg-[#2b3247] '>
+        <img src='/landingpage_2.png' alt='freelance-image' className='px-20' />
+        </div>
+      </section>
+
+
+
+      <section>
+  <div className="flex flex-col lg:flex-row p-4 sm:p-8 md:p-12 lg:p-20 mt-12 md:mt-20 lg:mt-28 arsenal-sc-regular">
+    <div className="mx-auto lg:ml-4 xl:ml-44">
+      <img 
+        src="/public/landingpage_bigimage.png" 
+        alt="professional image" 
+        className="w-full max-w-md lg:max-w-none h-auto"
+      />
+    </div>
+
+    <div className="text-center mx-auto mt-8 lg:mt-44 px-4">
+      <span className="font-bold text-2xl sm:text-3xl md:text-4xl">Bussiness Elavators and</span>
+      <div>
+        <span className="font-bold text-2xl sm:text-3xl md:text-4xl">Carrer Crafters</span>
+      </div>
+      <div className="pt-6 md:pt-12 text-lg md:text-2xl space-y-2">
+        <p className="max-w-md mx-auto">
+          Our being able to do what we like best, 
+          every pleasure is to be welcomed and every
+          pain avoided but in certain circumstances and 
+          owing to the claims of duty or the obligations.
+        </p>
+      </div>
+      <button
+        onClick={toRolePage}
+        className="bg-[#0000ff] w-44 rounded-small text-white font-bold hover:bg-green-500 h-10 sm:h-12 mt-6 sm:mt-8 md:mt-12"
+      >
+        Get Started
+      </button>
+    </div>
+  </div>
+</section>
+
+<section>
+  <div className='text-center min-h-[12rem] sm:min-h-[14rem] md:min-h-[17rem] w-full bg-[#2b3247] mt-12 sm:mt-20 md:mt-28 lg:mt-36 arsenal-sc-regular px-4 py-6 md:py-[4rem]'>
+    <ul className='grid gap-0.5 sm:gap-2 text-white max-w-4xl mx-auto'>
+      <li className='text-lg sm:text-2xl md:text-3xl font-medium mb-1 sm:mb-3'>Find an Expert for Anything which is Technological</li>
+      <li className='text-xs sm:text-sm md:text-md'>Work with highly-efficient freelance talent from around the country.</li>
+      <li className='text-xs sm:text-sm md:text-md'>Take control of your project with Devlink.</li>
+      <li className='text-xs sm:text-sm md:text-md'>Pay securely and confidently.</li>
+      <li className='text-xs sm:text-sm md:text-md'>No hillden fees.</li>
+    </ul>
+  </div>
+</section>
+      
+
       <Footer />
     </div>
   );
