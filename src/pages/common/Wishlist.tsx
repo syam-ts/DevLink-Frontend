39d38d9caf0fe83f30f98abec9f4a91815727b59
| ｓｙａｍ | <syamnandhu3@gmail.com>
2025-04-10 21:31:27 +0530

    fix: wishlist empty bug fix

--- src/pages/common/Wishlist.tsx
+++ src/pages/common/Wishlist.tsx
@@ -34,12 +34,12 @@ const Wishlist: React.FC = () => {
 
   useEffect(() => {
     try {
-      (async () => {
+      const fetchJobs = async () => {
         const { data } = await apiUserInstance.get("/wishlist");
         setJobs(data.wishlist.jobPostData);
-        console.log("twh: ", data.wishlist.jobPostData);
         setWishlistId(data.wishlist._id);
-      })();
+      }
+      fetchJobs();
       setRemoveFlag(false);
     } catch (err) {
       console.log("ERROR: ", err.message);
@@ -72,6 +72,7 @@ const Wishlist: React.FC = () => {
     }
   };
 
+
   return (
     <div className="w-full px-4 sm:px-6 md:px-8">
       <Sonner />
@@ -80,22 +81,21 @@ const Wishlist: React.FC = () => {
       <section>
         <div className="flex justify-center pt-12 sm:pt-16 md:pt-20 lg:pt-24">
           <span className="arsenal-sc-regular text-2xl sm:text-3xl">
-            {" "}
-            Wishlist{" "}
+            Wishlist
           </span>
         </div>
         <hr className="w-4/5 sm:w-2/3 mx-auto mt-2" />
       </section>
 
       {/* Wishlist Content */}
-      {Object.entries(jobs).length != 0 ? (
+      {jobs._id != "" ? (
         <div className="mt-8 sm:mt-12 md:mt-16 space-y-6 sm:space-y-8 md:space-y-10">
           {Object.entries(jobs)?.map(([key, job]: [string, Jobs]) => (
             <div
               key={key}
               className="w-full sm:w-11/12 md:w-5/6 lg:w-2/3 transform relative transition duration-500 hover:shadow-xl border-gray-100 shadow-lg rounded-2xl border mx-auto px-5 py-4 sm:p-6 md:p-8 lg:p-12 arsenal-sc-regular"
             >
-              <div className="flex flex-col lg:flex-row lg:justify-between gap-6"> 
+              <div className="flex flex-col lg:flex-row lg:justify-between gap-6">
                 <div className="flex-1">
                   <span className="text-xl sm:text-2xl mb-2 line-clamp-1">
                     {job?.title}
@@ -108,7 +108,7 @@ const Wishlist: React.FC = () => {
                     <span className="text-sm">{job?.expertLevel}</span>
                     <span className="text-sm">{job?.location}</span>
                   </div>
- 
+
                   <div className="flex flex-wrap gap-2 my-3">
                     {job?.requiredSkills?.map(
                       (skill: string, index: number) => (
@@ -122,7 +122,7 @@ const Wishlist: React.FC = () => {
                     )}
                   </div>
                 </div>
- 
+
                 <div className="flex flex-row lg:flex-col justify-between lg:text-end lg:min-w-40">
                   <div className="grid gap-1">
                     <span className="text-sm">{job?.amount}.00₹</span>
