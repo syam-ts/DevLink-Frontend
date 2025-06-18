876c61f587931884638c5e3b23c987bbf80dfb45
syam-ts <syamnandhu3@gmail.com>
2025-06-18 13:18:59 +0530

    fix: removed serachbar from client home

--- src/pages/client/HomeClient.tsx
+++ src/pages/client/HomeClient.tsx
@@ -135,45 +135,7 @@ const HomeClient = () => {
                   <br /> excellence and limitless opportunities.
                 </span>
               </div>
-              <label className="mx-auto mt-10 md:mt-16 relative max-md:hidden bg-white w-full max-w-xl md:max-w-2xl flex flex-col md:flex-row items-center justify-center border py-2 px-4 rounded-2xl gap-2 shadow-2xl focus-within:border-gray-300">
-                <input
-                  id="search-bar"
-                  placeholder="Search for freelancers..."
-                  className="px-6 py-2 w-full text-black rounded-md flex-1 outline-none bg-white"
-                />
-                <button className="w-full md:w-auto px-6 py-3 bg-black text-white border-black border rounded-xl transition-all duration-300 hover:scale-95 disabled:opacity-70">
-                  <div className="relative flex items-center justify-center">
-                    <div className="flex items-center justify-center h-3 w-3 absolute inset-1/2 -translate-x-1/2 -translate-y-1/2 transition-all">
-                      <svg
-                        className="opacity-0 animate-spin w-full h-full"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                      >
-                        <circle
-                          className="opacity-25"
-                          cx="12"
-                          cy="12"
-                          r="10"
-                          stroke="currentColor"
-                          strokeWidth="4"
-                        ></circle>
-                        <path
-                          className="opacity-75"
-                          fill="currentColor"
-                          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                        ></path>
-                      </svg>
-                    </div>
-
-                    <div className="flex items-center transition-all opacity-1">
-                      <span className="text-sm whitespace-nowrap mx-auto">
-                        Search
-                      </span>
-                    </div>
-                  </div>
-                </button>
-              </label>
+       
             </div>
           </figcaption>
         </figure>
