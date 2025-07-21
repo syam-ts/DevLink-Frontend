11d2778eb7c821b17ec1e713f5d8db16a3c57d26
syam-ts <syamnandhu3@gmail.com>
2025-07-21 23:27:02 +0530

    feat: add api services

--- src/services/api/userServices.ts
+++ src/services/api/userServices.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import config from "../../config/helper/config";
+import { apiUserInstance } from "@/api/axiosInstance/axiosUserInstance";
 
 export const UserService = {
     loginUser: async (email: string, password: string) => {
@@ -21,20 +22,31 @@ export const UserService = {
         }
     },
 
-    signupUser: async (
-        name: string,
-        password: string,
-        email: string, 
-    ) => {
+    signupUser: async (name: string, password: string, email: string) => {
         try {
             const { data } = await axios.post(
                 `${config.VITE_SERVER_URL}/user/signup`,
                 {
                     name,
                     password,
-                    email, 
+                    email,
                 }
             );
+
+            return data;
+        } catch (error: unknown) {
+            const err = error as { response: { data: { success: boolean } } };
+            if (!err.response.data.success) {
+                return err.response.data;
+            }
+        }
+    },
+
+    getClients: async () => {
+        try {
+            const { data } = await apiUserInstance.get("/getHome", {
+                withCredentials: true,
+            });
 
             return data;
         } catch (error: unknown) {
