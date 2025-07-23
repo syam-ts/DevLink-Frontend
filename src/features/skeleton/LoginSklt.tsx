7959a1c197778b1a1587cb436d38957669785e78
syam-ts <syamnandhu3@gmail.com>
2025-07-23 20:59:25 +0530

    feat: add skeleton

---
+++ src/features/skeleton/LoginSklt.tsx
@@ -0,0 +1,12 @@
+function LoginSklt() {
+    return (
+        <div className="bg-gray-500 h-screen w-screen">
+            <div className="bg-gray-500 ">
+                <p className="bg-gray-500 "></p>
+            </div>
+            <div className="bg-gray-500 "></div>
+        </div>
+    );
+}
+
+export default LoginSklt;
